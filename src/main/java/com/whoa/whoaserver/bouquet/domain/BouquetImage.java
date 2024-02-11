7a25695618f009d9556c7269725a8a2ea8713217
Subin <rla0010101@gmail.com>
2024-02-11 21:33:24 +0900

    feat: BouquetImage Builder, create 메소드 추가

--- src/main/java/com/whoa/whoaserver/bouquet/domain/BouquetImage.java
+++ src/main/java/com/whoa/whoaserver/bouquet/domain/BouquetImage.java
@@ -2,6 +2,7 @@ package com.whoa.whoaserver.bouquet.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,7 +14,6 @@ public class BouquetImage {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "image_id")
     private Long id;
 
     @Column(unique = true, nullable = false)
@@ -23,4 +23,17 @@ public class BouquetImage {
      * 다른 엔티티에서 해당 이미지를 사용할 때 true
      */
     private boolean used;
+
+    @Builder
+    private BouquetImage(String fileName, boolean used) {
+        this.fileName = fileName;
+        this.used = used;
+    }
+
+    public static BouquetImage create(String fileName) {
+        return BouquetImage.builder()
+                .fileName(fileName)
+                .used(false)
+                .build();
+    }
 }
