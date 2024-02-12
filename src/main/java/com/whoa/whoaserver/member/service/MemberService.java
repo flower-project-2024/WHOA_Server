309623e9f92ba6864ae0a0d6498e1ced33b0b6a1
Subin <rla0010101@gmail.com>
2024-02-12 14:09:20 +0900

    feat: MemberService

--- src/main/java/com/whoa/whoaserver/member/service/MemberService.java
+++ src/main/java/com/whoa/whoaserver/member/service/MemberService.java
@@ -2,9 +2,15 @@ package com.whoa.whoaserver.member.service;
 
 import lombok.RequiredArgsConstructor;
 
+import static com.whoa.whoaserver.global.exception.ExceptionCode.EXIST_MEMBER;
+
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.whoa.whoaserver.global.exception.BadRequestException;
+import com.whoa.whoaserver.member.domain.Member;
 import com.whoa.whoaserver.member.domain.MemberRepository;
 import com.whoa.whoaserver.member.dto.request.MemberRegisterRequest;
 import com.whoa.whoaserver.member.dto.response.MemberInfo;
@@ -17,6 +23,18 @@ public class MemberService {
 
     @Transactional
     public MemberInfo register(MemberRegisterRequest request) {
-        return MemberInfo.of(null);
+
+        Optional<Member> optionalMember = memberRepository.findByDeviceId(request.deviceId());
+
+        if (optionalMember.isPresent()) {
+            throw new BadRequestException(EXIST_MEMBER);
+        }
+
+        Member newMember = new Member();
+        newMember.init(request.deviceId());
+
+        memberRepository.save(newMember);
+        
+        return MemberInfo.of(newMember);
     }
 }
