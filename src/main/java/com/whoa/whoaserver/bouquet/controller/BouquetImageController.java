fe66876574f034c0a6a98963ed64c2e4d7ad5c93
Subin <rla0010101@gmail.com>
2024-02-17 17:35:15 +0900

    feat: BouquetImageController

--- src/main/java/com/whoa/whoaserver/bouquet/controller/BouquetImageController.java
+++ src/main/java/com/whoa/whoaserver/bouquet/controller/BouquetImageController.java
@@ -2,6 +2,8 @@ package com.whoa.whoaserver.bouquet.controller;
 
 import java.net.URL;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,12 +27,12 @@ import lombok.RequiredArgsConstructor;
 public class BouquetImageController {
     
     private final BouquetImageService bouquetImageService;
-    
+
     @PostMapping("/presigned_url")
-    @Operation(summary = "이미지 등록 주소", description = "S3 이미지 저장을 위한 주소를 반환합니다.")
-    public ResponseEntity<PresignedUrlResponse> providePresignedUrl(@DeviceUser UserContext userContext, @RequestBody PresignedUrlRequest request) {
+    @Operation(summary = "이미지 등록 주소", description = "Header에 MEMBER_ID(key), 디바이스 등록 이후 반환 받은 id(value)로 요청하면 S3 이미지 저장을 위한 주소를 반환합니다.")
+    public ResponseEntity<PresignedUrlResponse> providePresignedUrl(@DeviceUser UserContext userContext) {
         
-        URL presignedUrl = bouquetImageService.createPresignedUrl(userContext, request);
+        URL presignedUrl = bouquetImageService.createPresignedUrl(userContext);
 
         return ResponseEntity.ok(PresignedUrlResponse.create(presignedUrl));
     }
