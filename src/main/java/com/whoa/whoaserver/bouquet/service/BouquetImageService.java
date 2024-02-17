ea8ea057fc7af90aba54e9af895a527b621e27ff
Subin <rla0010101@gmail.com>
2024-02-17 17:29:54 +0900

    feat: BouquetImageService

--- src/main/java/com/whoa/whoaserver/bouquet/service/BouquetImageService.java
+++ src/main/java/com/whoa/whoaserver/bouquet/service/BouquetImageService.java
@@ -1,15 +1,26 @@
 package com.whoa.whoaserver.bouquet.service;
 
+import static com.whoa.whoaserver.global.exception.ExceptionCode.IMAGE_EXTENSION_NOT_SUPPORTED;
+import static com.whoa.whoaserver.global.exception.ExceptionCode.IMAGE_SIZE_LIMIT_ERROR;
+
 import java.net.URL;
+import java.time.Duration;
+import java.util.function.Consumer;
 
 import org.springframework.stereotype.Service;
 
 import com.whoa.whoaserver.bouquet.domain.BouquetImageRepository;
 import com.whoa.whoaserver.bouquet.dto.request.PresignedUrlRequest;
 import com.whoa.whoaserver.global.dto.UserContext;
+import com.whoa.whoaserver.global.exception.BadRequestException;
+import com.whoa.whoaserver.global.properties.S3Properties;
+import com.whoa.whoaserver.global.extension.Extension;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest.Builder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,13 +29,44 @@ import org.slf4j.LoggerFactory;
 @Transactional
 @RequiredArgsConstructor
 public class BouquetImageService {
-    
-    private final Logger logger = LoggerFactory.getLogger(BouquetImageService.class);
     private final BouquetImageRepository bouquetImageRepository;
+    private final S3Properties s3Properties;
+    private final S3Presigner s3Presigner;
 
-    public URL createPresignedUrl(UserContext userContext, PresignedUrlRequest request) {
+    public URL createPresignedUrl(UserContext userContext) {
         Long memberId = userContext.id();
-        logger.info("Member ID: {}", memberId);
-        throw new UnsupportedOperationException("Unimplemented method 'createPresignedUrl'");
+        Long contentLength = 123L;
+
+        String fileName = generateFileName(memberId);
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(r -> 
+            r.signatureDuration(Duration.ofSeconds(s3Properties.presignedExpires()))
+                .putObjectRequest(createPutObjectRequest(contentLength, fileName)));
+        
+        return presignedRequest.url();
+        
     }
+
+    private void validateExtension(String extensionValue) {
+        try {
+            Extension extension = Extension.valueOf(extensionValue.toUpperCase());
+
+            if (!extension.isImageType()) {
+                throw new BadRequestException(IMAGE_EXTENSION_NOT_SUPPORTED);
+            }
+        } catch (IllegalArgumentException e) {
+            throw new BadRequestException(IMAGE_EXTENSION_NOT_SUPPORTED);
+        }
+    }
+
+    private String generateFileName(Long memberId) {
+
+        return memberId.toString();
+    }
+
+    private Consumer<Builder> createPutObjectRequest(long contentLength, String fileName) {
+        return objectRequest -> objectRequest.bucket(s3Properties.bucket())
+            .contentLength(contentLength)
+            .key(fileName);
+    }
+    
 }
