23c7e8174be73d97ef02466af4ed7d3a66f2215f
Subin <rla0010101@gmail.com>
2024-02-24 23:57:55 +0900

    refact: bouquet id 반환으로 controller, service 수정

--- src/main/java/com/whoa/whoaserver/bouquet/controller/BouquetCustomizingController.java
+++ src/main/java/com/whoa/whoaserver/bouquet/controller/BouquetCustomizingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.whoa.whoaserver.bouquet.dto.request.BouquetCustomizingRequest;
+import com.whoa.whoaserver.bouquet.dto.response.BouquetCustomizingResponse;
 import com.whoa.whoaserver.bouquet.service.BouquetCustomizingService;
 import com.whoa.whoaserver.global.annotation.DeviceUser;
 import com.whoa.whoaserver.global.dto.UserContext;
@@ -33,10 +34,10 @@ public class BouquetCustomizingController {
     @PostMapping("/customizing")
     @Operation(summary = "꽃다발 제작", description = "Header에 MEMBER_ID(key), 디바이스 등록 이후 반환 받은 id(value)로 요청하면 꽃다발 주문을 등록합니다.")
 
-    public ResponseEntity<String> registerBouquet(@DeviceUser UserContext userContext, @Valid @RequestBody BouquetCustomizingRequest request) { 
+    public ResponseEntity<BouquetCustomizingResponse> registerBouquet(@DeviceUser UserContext userContext, @Valid @RequestBody BouquetCustomizingRequest request) { 
         Long memberId = userContext.id();
-        bouquetCustomizingService.registerBouquet(request, memberId);
-        return new ResponseEntity<>("ok", HttpStatus.OK);
+        BouquetCustomizingResponse response = bouquetCustomizingService.registerBouquet(request, memberId);
+        return ResponseEntity.ok(response);
     }
     
 }
--- src/main/java/com/whoa/whoaserver/bouquet/service/BouquetCustomizingService.java
+++ src/main/java/com/whoa/whoaserver/bouquet/service/BouquetCustomizingService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.whoa.whoaserver.bouquet.domain.Bouquet;
 import com.whoa.whoaserver.bouquet.domain.BouquetRepository;
 import com.whoa.whoaserver.bouquet.dto.request.BouquetCustomizingRequest;
+import com.whoa.whoaserver.bouquet.dto.response.BouquetCustomizingResponse;
 import com.whoa.whoaserver.global.exception.BadRequestException;
 import com.whoa.whoaserver.global.exception.ExceptionCode;
 import com.whoa.whoaserver.member.domain.Member;
@@ -23,13 +24,15 @@ public class BouquetCustomizingService {
     private final MemberRepository memberRepository;
     private final BouquetRepository bouquetRepository;
 
-    public void registerBouquet(BouquetCustomizingRequest request, Long memberId) {
+    public BouquetCustomizingResponse registerBouquet(BouquetCustomizingRequest request, Long memberId) {
 
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new BadRequestException(ExceptionCode.INVALID_MEMBER));
 
         Bouquet bouquet = createBouquetEntity(request, member);
 
         bouquetRepository.save(bouquet);
+
+        return BouquetCustomizingResponse.of(bouquet);
     }
 
     private Bouquet createBouquetEntity(BouquetCustomizingRequest request, Member member) {
