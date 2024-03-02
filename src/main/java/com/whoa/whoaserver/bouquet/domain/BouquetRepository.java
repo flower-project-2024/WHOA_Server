8f02fd96e8ea286c12c8fd560e1692c3ef60ce93
Subin <rla0010101@gmail.com>
2024-03-02 15:56:27 +0900

    feat: 멤버의 꽃다발 삭제를 위한 BouquetRepository 메소드 추가

--- src/main/java/com/whoa/whoaserver/bouquet/domain/BouquetRepository.java
+++ src/main/java/com/whoa/whoaserver/bouquet/domain/BouquetRepository.java
@@ -2,6 +2,8 @@ package com.whoa.whoaserver.bouquet.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BouquetRepository extends JpaRepository<Bouquet, Long>{
-    
+    Optional<Bouquet> findByMemberIdAndId(Long memberId, Long bouquetId);
 }
