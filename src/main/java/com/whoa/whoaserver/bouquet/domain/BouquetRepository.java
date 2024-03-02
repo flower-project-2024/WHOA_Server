e518c2236ef617c6b21aec474cefeaac9c0eaf97
Subin <rla0010101@gmail.com>
2024-03-02 17:05:43 +0900

    feat: 멤버의 저장된 꽃다발 주문 요구서 전체 조회를 위한 repository 메소드 추가

--- src/main/java/com/whoa/whoaserver/bouquet/domain/BouquetRepository.java
+++ src/main/java/com/whoa/whoaserver/bouquet/domain/BouquetRepository.java
@@ -2,8 +2,11 @@ package com.whoa.whoaserver.bouquet.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BouquetRepository extends JpaRepository<Bouquet, Long>{
     Optional<Bouquet> findByMemberIdAndId(Long memberId, Long bouquetId);
+
+    Optional<List<Bouquet>> findByMemberId(Long memberId);
 }
