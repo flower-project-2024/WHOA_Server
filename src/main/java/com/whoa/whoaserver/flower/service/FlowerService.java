a2016bdb7fbfa0e18bf253cd15cd944e9c1cf0cd
Subin <rla0010101@gmail.com>
2024-03-09 12:07:11 +0900

    feat: 꽃 검색 FlowerService getAllFlowers, convertToDto 메소드 추가

--- src/main/java/com/whoa/whoaserver/flower/service/FlowerService.java
+++ src/main/java/com/whoa/whoaserver/flower/service/FlowerService.java
@@ -2,10 +2,14 @@ package com.whoa.whoaserver.flower.service;
 
 import com.whoa.whoaserver.flower.domain.Flower;
 import com.whoa.whoaserver.flower.dto.FlowerResponseDto;
+import com.whoa.whoaserver.flower.dto.FlowerSearchResponseDto;
 import com.whoa.whoaserver.flower.repository.FlowerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +28,21 @@ public class FlowerService {
         String recommendDate = month + "/" + date;
         Flower recommendFlower = flowerRepository.findFlowerByRecommendDate(recommendDate);
         return FlowerResponseDto.of(recommendFlower);
+    }
+
+    @Transactional
+    public List<FlowerSearchResponseDto> getAllFlowers() {
+        List<Flower> flowers = flowerRepository.findAll();
+
+        return flowers.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    private FlowerSearchResponseDto convertToDto(Flower flower) {
+        return new FlowerSearchResponseDto(
+                flower.getFlowerId(),
+                flower.getFlowerName()
+        );
     }
 }
