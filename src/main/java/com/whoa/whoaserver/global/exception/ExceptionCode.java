6c985fbfe6366581760ef7bace362dd6ba58dc36
Subin <rla0010101@gmail.com>
2024-03-14 20:26:08 +0900

    refact: ExceptionCode Enum HttpStatus와 String으로 수정

--- src/main/java/com/whoa/whoaserver/global/exception/ExceptionCode.java
+++ src/main/java/com/whoa/whoaserver/global/exception/ExceptionCode.java
@@ -2,22 +2,21 @@ package com.whoa.whoaserver.global.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 @Getter
 public enum ExceptionCode {
 
-    INVALID_REQUEST(1000, "올바르지 않은 요청입니다."),
-    FAIL_TO_GET_GOOGLE_MEMBER(1001, "구글 회원 정보를 가져오지 못했습니다."),
-    EXIST_MEMBER(1002, "존재하는 회원입니다."),
-    INVALID_MEMBER(1003, "존재하지 않는 회원입니다."),
+    EXIST_MEMBER(HttpStatus.CONFLICT, "이미 존재하는 회원입니다."),
+    INVALID_MEMBER(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
 
-    NOT_REGISTER_BOUQUET(2000, "등록되지 않은 꽃다발 주문서입니다."),
-    NOT_MEMBER_BOUQUET(2001, "해당 유저가 주문한 꽃다발이 아닙니다."),
+    NOT_REGISTER_BOUQUET(HttpStatus.NOT_FOUND, "등록되지 않은 꽃다발 주문서입니다."),
+    NOT_MEMBER_BOUQUET(HttpStatus.CONFLICT, "해당 유저가 주문한 꽃다발이 아닙니다."),
 
-    IMAGE_SIZE_LIMIT_ERROR(3000, "이미지의 크기가 기준을 초과합니다."),
-    IMAGE_EXTENSION_NOT_SUPPORTED(3001, "지원하지 않는 이미지 파일 형식입니다.");
+    IMAGE_SIZE_LIMIT_ERROR(HttpStatus.FORBIDDEN, "이미지의 크기가 기준을 초과합니다."),
+    IMAGE_EXTENSION_NOT_SUPPORTED(HttpStatus.BAD_REQUEST, "지원하지 않는 이미지 파일 형식입니다.");
 
-    private final int code;
+    private final HttpStatus status;
     private final String message;
 }
