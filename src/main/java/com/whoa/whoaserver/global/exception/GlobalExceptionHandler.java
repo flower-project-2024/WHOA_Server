751398e5056297c1c5b7854c380ed06faf2bd7db
Subin <rla0010101@gmail.com>
2024-03-14 20:33:40 +0900

    fix, refact: GlobalExceptionHandler 공통 전역 예외처리 로직 수정

--- src/main/java/com/whoa/whoaserver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/whoa/whoaserver/global/exception/GlobalExceptionHandler.java
@@ -1,29 +1,36 @@
 package com.whoa.whoaserver.global.exception;
 
-import jakarta.security.auth.message.AuthException;
+import com.whoa.whoaserver.global.dto.common.CommonResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Objects;
 
-import static com.whoa.whoaserver.global.exception.ExceptionCode.INVALID_REQUEST;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException e,
+            HttpStatusCode status) {
         final String errMessage = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
-        return ResponseEntity.badRequest()
-                .body(new ExceptionResponse(INVALID_REQUEST.getCode(), errMessage));
+        final ExceptionResponse exceptionResponse = ExceptionResponse.of(e.getClass().getSimpleName(), errMessage);
+        CommonResponse response = CommonResponse.fail(status.value(), exceptionResponse);
+        return ResponseEntity.status(status).body(response);
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ExceptionResponse> handleBadRequestException(final BadRequestException e) {
-        return ResponseEntity.badRequest()
-                .body(new ExceptionResponse(e.getCode(), e.getMessage()));
+    public ResponseEntity<CommonResponse> handleBadRequestException(final BadRequestException e) {
+        final ExceptionCode exceptionCode = e.getExceptionCode();
+        final ExceptionResponse exceptionResponse = ExceptionResponse.of(exceptionCode.name(), exceptionCode.getMessage());
+        final CommonResponse response = CommonResponse.fail(exceptionCode.getStatus().value(), exceptionResponse);
+        return ResponseEntity.status(exceptionCode.getStatus()).body(response);
     }
 
 }
