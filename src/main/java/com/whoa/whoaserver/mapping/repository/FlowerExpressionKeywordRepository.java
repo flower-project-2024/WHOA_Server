f28d0778f1aeb73b1325658d74f86de3c71e8ea0
Subin <rla0010101@gmail.com>
2024-06-24 17:46:43 +0900

    feat[#60]: 매핑테이블 querydsl 기반 repository 구현

--- src/main/java/com/whoa/whoaserver/mapping/repository/FlowerExpressionKeywordRepository.java
+++ src/main/java/com/whoa/whoaserver/mapping/repository/FlowerExpressionKeywordRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface FlowerExpressionKeywordRepository extends JpaRepository<FlowerExpressionKeyword, Long> {
+public interface FlowerExpressionKeywordRepository extends JpaRepository<FlowerExpressionKeyword, Long>, FlowerExpressionKeywordRepositoryCustom {
 
     List<FlowerExpressionKeyword> findAllByKeyword_KeywordId(Long keywordId);
 }
