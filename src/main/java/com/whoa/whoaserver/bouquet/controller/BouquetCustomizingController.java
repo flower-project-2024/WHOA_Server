b991c5960c96565ab2d7f4de0c240443566458c3
Subin <rla0010101@gmail.com>
2024-08-28 17:41:40 +0900

    feat: 1차 MVP, 2차 MVP 버전 분리

--- src/main/java/com/whoa/whoaserver/bouquet/controller/BouquetCustomizingController.java
+++ src/main/java/com/whoa/whoaserver/bouquet/controller/BouquetCustomizingController.java
@@ -1,10 +1,6 @@
 package com.whoa.whoaserver.bouquet.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.whoa.whoaserver.bouquet.dto.response.BouquetInfoDetailResponse;
-import com.whoa.whoaserver.global.exception.WhoaException;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,11 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-
-import static com.whoa.whoaserver.global.exception.ExceptionCode.*;
 
 @Tag(name = "Bouquet Customizing", description = "Header에 MEMBER_ID(key), 디바이스 등록 이후 반환 받은 id(value)로 요청해주세요.")
 @RestController
@@ -32,45 +23,14 @@ import static com.whoa.whoaserver.global.exception.ExceptionCode.*;
 public class BouquetCustomizingController {
 
     private final BouquetCustomizingService bouquetCustomizingService;
-	private final ObjectMapper objectMapper;
 
-    @PostMapping(value ="/customizing", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @Operation(summary = "꽃다발 제작", description = "꽃다발 주문과 이미지를 등록합니다.")
-    public ResponseEntity<BouquetCustomizingResponse> registerBouquet(@DeviceUser UserContext userContext,
-																	  @Valid @RequestPart("request") String bouquetRequest,
-																	  @RequestPart("imgUrl") List<MultipartFile> multipartFiles) {
-
-		try {
-			Long memberId = userContext.id();
-			BouquetCustomizingRequest request = objectMapper.readValue(bouquetRequest, BouquetCustomizingRequest.class);
-			BouquetCustomizingResponse response = bouquetCustomizingService.registerBouquet(request, memberId, multipartFiles);
-			return ResponseEntity.ok(response);
-		} catch (JsonProcessingException e) {
-			throw new WhoaException(INVALID_BOUQUET_REQUEST_JSON_FORMAT);
-		} catch (Exception e) {
-			throw new WhoaException(IMAGE_UPLOAD_ERROR);
-		}
-
-    }
-
-    @PutMapping("/customizing/{bouquetId}")
-    @Operation(summary = "꽃다발 수정", description = "유저의 꽃다발 주문서와 이미지를 수정합니다.")
-    public ResponseEntity<BouquetCustomizingResponse> updateBouquet(@DeviceUser UserContext userContext,
-																	@Valid @RequestPart("request") String bouquetRequest,
-																	@RequestPart("imgUrl") List<MultipartFile> multipartFiles,
-																	@PathVariable("bouquetId") final Long bouquetId) {
-
-		try {
-			Long memberId = userContext.id();
-			BouquetCustomizingRequest request = objectMapper.readValue(bouquetRequest, BouquetCustomizingRequest.class);
-			BouquetCustomizingResponse response = bouquetCustomizingService.updateBouquet(request, memberId, bouquetId, multipartFiles);
-			return ResponseEntity.ok(response);
-		} catch (JsonProcessingException e) {
-			throw new WhoaException(INVALID_BOUQUET_REQUEST_JSON_FORMAT);
-		} catch (Exception e) {
-			throw new WhoaException(IMAGE_UPLOAD_ERROR);
-		}
-    }
+	@PostMapping("/customizing")
+	@Operation(summary = "꽃다발 제작", description = "꽃다발 주문을 등록합니다.")
+	public ResponseEntity<BouquetCustomizingResponse> registerBouquet(@DeviceUser UserContext userContext, @Valid @RequestBody BouquetCustomizingRequest request) {
+		Long memberId = userContext.id();
+		BouquetCustomizingResponse response = bouquetCustomizingService.registerBouquet(request, memberId);
+		return ResponseEntity.ok(response);
+	}
 
     @DeleteMapping("/{bouquetId}")
     @Operation(summary = "꽃다발 주문서 단건 삭제", description = "유저의 꽃다발 주문서 하나를 삭제합니다.")
---
+++ src/main/java/com/whoa/whoaserver/bouquet/service/BouquetCustomizingServiceV2.java
@@ -0,0 +1,118 @@
+package com.whoa.whoaserver.bouquet.service;
+
+import com.whoa.whoaserver.bouquet.domain.Bouquet;
+import com.whoa.whoaserver.bouquet.domain.BouquetImage;
+import com.whoa.whoaserver.bouquet.dto.request.BouquetCustomizingRequest;
+import com.whoa.whoaserver.bouquet.dto.response.BouquetCustomizingResponseV2;
+import com.whoa.whoaserver.bouquet.repository.BouquetImageRepository;
+import com.whoa.whoaserver.bouquet.repository.BouquetRepository;
+import com.whoa.whoaserver.global.config.S3Config;
+import com.whoa.whoaserver.global.exception.ExceptionCode;
+import com.whoa.whoaserver.global.exception.WhoaException;
+import com.whoa.whoaserver.member.domain.Member;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.whoa.whoaserver.global.exception.ExceptionCode.DUPLICATED_BOUQUET_NAME;
+import static com.whoa.whoaserver.global.exception.ExceptionCode.NOT_MEMBER_BOUQUET;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BouquetCustomizingServiceV2 {
+
+	private final BouquetRepository bouquetRepository;
+	private final BouquetImageRepository bouquetImageRepository;
+	private final BouquetCustomizingService bouquetCustomizingService;
+	private final S3Config s3Config;
+
+	public BouquetCustomizingResponseV2 registerBouquet(BouquetCustomizingRequest request, Long memberId, List<MultipartFile> multipartFiles) {
+
+		Member member = bouquetCustomizingService.getMemberByMemberId(memberId);
+
+		Optional<Bouquet> existingBouquetOptional = bouquetRepository.findByMemberIdAndBouquetName(memberId, request.bouquetName());
+
+		if (existingBouquetOptional.isPresent()) {
+			throw new WhoaException(DUPLICATED_BOUQUET_NAME);
+		}
+
+		Bouquet newBouquet = createBouquetEntity(request, member);
+
+		bouquetRepository.save(newBouquet);
+
+		List<String> imgPaths = handleMultipartFiles(memberId, newBouquet, multipartFiles);
+
+		return BouquetCustomizingResponseV2.of(newBouquet, imgPaths);
+	}
+
+	private Bouquet createBouquetEntity(BouquetCustomizingRequest request, Member member) {
+		return Bouquet.orderBouquet(
+			member,
+			request.bouquetName(),
+			request.purpose(),
+			request.colorType(),
+			request.colorName(),
+			request.pointColor(),
+			request.flowerType(),
+			request.substitutionType(),
+			request.wrappingType(),
+			request.price(),
+			request.requirement()
+		);
+	}
+
+	private List<String> handleMultipartFiles(Long memberId, Bouquet bouquet, List<MultipartFile> multipartFiles) {
+		List<String> imgPaths = s3Config.upload(multipartFiles);
+		saveMultipleFilesUrlWithBouquetAtOnce(memberId, imgPaths, bouquet.getId());
+
+		return imgPaths;
+	}
+
+	private void saveMultipleFilesUrlWithBouquetAtOnce(Long memberId, List<String> imgPaths, Long bouquetId) {
+
+		Bouquet bouquetWithImg = bouquetRepository.findByBouquetIdWithMember(memberId, bouquetId)
+			.orElseThrow(() -> new WhoaException(ExceptionCode.NOT_REGISTER_BOUQUET));
+
+		for (String imgUrl : imgPaths) {
+			BouquetImage bouquetImage = BouquetImage.create(bouquetWithImg, imgUrl);
+			bouquetImageRepository.save(bouquetImage);
+		}
+	}
+
+	public BouquetCustomizingResponseV2 updateBouquet(BouquetCustomizingRequest request, Long memberId, Long bouquetId, List<MultipartFile> multipartFiles) {
+		Member member = bouquetCustomizingService.getMemberByMemberId(memberId);
+
+		Bouquet existingBouquet = bouquetCustomizingService.getBouquetByMemberIdAndBouquetId(memberId, bouquetId);
+
+		if (!existingBouquet.getMember().equals(member)) {
+			throw new WhoaException(NOT_MEMBER_BOUQUET);
+		}
+
+		existingBouquet.changeBouquet(
+			request.bouquetName(),
+			request.purpose(),
+			request.colorType(),
+			request.colorName(),
+			request.pointColor(),
+			request.flowerType(),
+			request.substitutionType(),
+			request.wrappingType(),
+			request.price(),
+			request.requirement()
+		);
+
+		bouquetRepository.save(existingBouquet);
+
+		List<BouquetImage> existingBouquetImages = bouquetImageRepository.findAllByBouquet(existingBouquet);
+		bouquetImageRepository.deleteAll(existingBouquetImages);
+
+		List<String> imgPaths = handleMultipartFiles(memberId, existingBouquet, multipartFiles);
+
+		return BouquetCustomizingResponseV2.of(existingBouquet, imgPaths);
+	}
+}
