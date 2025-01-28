f593a746e3f3a0d33fbb47c051981b3f32ac7999
Subin <rla0010101@gmail.com>
2025-01-28 10:50:21 +0900

    chore: customException whoaException 추가

--- src/main/java/com/whoa/whoaserver/global/exception/WhoaException.java
+++ src/main/java/com/whoa/whoaserver/global/exception/WhoaException.java
@@ -2,6 +2,8 @@ package com.whoa.whoaserver.global.exception;
 
 import lombok.Getter;
 
+import static com.whoa.whoaserver.global.utils.LoggerUtils.logger;
+
 @Getter
 public class WhoaException extends RuntimeException {
 
@@ -11,4 +13,11 @@ public class WhoaException extends RuntimeException {
         super(exceptionCode.getMessage());
         this.exceptionCode = exceptionCode;
     }
+
+	public WhoaException(ExceptionCode exceptionCode, String errorCause, String clientIP) {
+		super(exceptionCode.getMessage());
+		this.exceptionCode = exceptionCode;
+		logger.error("CustomException occurred : requestAddress = {}, errorMethod = {}, ExceptionCode = {}, Message = {}",
+			clientIP, errorCause, exceptionCode, exceptionCode.getMessage());
+	}
 }
